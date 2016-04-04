397fc727ba282cc7e8fbef18c3e5cbbfffed520f
JeongJiHoon <jhjeong@image.korea.ac.kr>
2016-04-04 16:55:46 +0900

    update

--- BMI_modules/_Developing/prep_rejectArtifactMAxMin.m
+++ BMI_modules/_Developing/prep_rejectArtifactMAxMin.m
@@ -1,12 +1,19 @@
-function [ rejected_data ] = prep_rejectArtifactMAxMin( data, varargin )
+function [ out ] = prep_rejectArtifactMAxMin( dat, varargin )
 
-dat = data;
+% dat = data;
 opt = opt_cellToStruct(varargin{:});
 % if isfield(opt.threshold)
 %     warning('Please input the value of threshold');
 % end
-
+% if ~isfield(opt)
+%     warning ('There is no threshold, Please input the value of threshold');
+% end
 %Find the channel index
+if ~isempty(opt.Ival)
+    dat.x = dat.x(str2double(opt.Ival(1)):str2double(opt.Ival(2)),:,:);
+end
+    
+
 if ~isempty(opt.channel)
     if opt.channel{1} == ':'
         for chInx = 1: size(dat.chSet,2)
@@ -17,7 +24,7 @@ if ~isempty(opt.channel)
             for chInx = 1: size(dat.chSet,2)
                 if strcmp(opt.channel{num}, dat.chSet{chInx}) == 1
                     selectChanInx(1,num) = chInx;
-                end
+                end 
             end
         end     
     end
@@ -34,29 +41,56 @@ rejmin = min(rejArtifactData, [], 1);
 rejcrt = rejmax-rejmin;
 rejcrt = reshape(rejcrt , [size(selectData,2) , size(selectData,3)]);
 inx = 0;
-for maxnum=1: size(selectData,3)
-    valMaxTrial(1,maxnum) =max(rejcrt(:,maxnum)); 
-    if valMaxTrial(maxnum) > str2double(opt.threshold)
-        inx = inx+1;
-        rejectTrial(1,inx) = maxnum;
-        rejectTrial(2,inx) = valMaxTrial(maxnum);
-    end
+
+for i =1 : size(selectData,3)
+    valMaxTrial(:,i) = max(rejcrt(:,i));
 end
-ratio = size(rejArtifact,3)*(str2double(opt.ratio)/100);
-sortrejData = sort(rejectTrial(2,:));
-if length(rejectTrial)> ratio
-    for num=1:size(sortrejData,2)
-        sortRejTrialInx(1,num) = rejectTrial(1,find(sortrejData(num) == rejectTrial(2,:)));
+if valMaxTrial <= str2double(opt.threshold)
+    out.x = dat.x;
+else
+    for maxnum=1: size(selectData,3)
+%         valMaxTrial(1,maxnum) =max(rejcrt(:,maxnum));
+        if valMaxTrial(maxnum) > str2double(opt.threshold)
+            inx = inx+1;
+            rejectTrial(1,inx) = maxnum;
+            rejectTrial(2,inx) = valMaxTrial(maxnum);
+        end
     end
-    sortRejTrial = [sortRejTrialInx;sortrejData];
-    findRejTrial = sortRejTrial(:,(length(rejectTrial)-ratio)+1:size(rejectTrial,2));
-end
-srtRejectTrial = sort(findRejTrial(1,:));
-TrialInx = [1:size(dat.x,2)];
-exctTrialIndex = setdiff(TrialInx,srtRejectTrial);
-for dataTrial = 1: size(exctTrialIndex,2) 
-    rejected_data(:,dataTrial,:)  = dat.x(:,exctTrialIndex(dataTrial),:);
+    ratio = round(size(rejArtifact,3)*(str2double(opt.ratio)/100));
+    sortrejData = sort(rejectTrial(2,:));
+    if length(rejectTrial)> ratio
+        for num=1:size(sortrejData,2)
+            A = find(sortrejData(num) == rejectTrial(2,:));
+            if length(A) >= 2
+                sortRejTrialInx(1,num) = rejectTrial(1,A(1));
+                if sortRejTrialInx(num) == sortRejTrialInx(num-1)
+                    sortRejTrialInx(1,num) = rejectTrial(1,A(2));
+                end
+            else
+                sortRejTrialInx(1,num) = rejectTrial(1,A);
+            end
+        end
+        sortRejTrial = [sortRejTrialInx;sortrejData];
+        findRejTrial = sortRejTrial(:,(length(rejectTrial)-ratio)+1:size(rejectTrial,2));
+        srtRejectTrial = sort(findRejTrial(1,:));
+    else
+        for num=1:size(sortrejData,2)
+            sortRejTrialInx(1,num) = rejectTrial(1,find(sortrejData(num) == rejectTrial(2,:)));
+        end
+        sortRejTrial = [sortRejTrialInx;sortrejData];
+        srtRejectTrial = sort(sortRejTrial(1,:));
+    end
+    
+    TrialInx = [1:size(dat.x,2)];
+    exctTrialIndex = setdiff(TrialInx,srtRejectTrial);
+    for dataTrial = 1: size(exctTrialIndex,2)
+        out.x(:,dataTrial,:)  = dat.x(:,exctTrialIndex(dataTrial),:);
+    end
 end
 
-% filteredData = dat.x(:,)
+
+
+
+end
+
 
