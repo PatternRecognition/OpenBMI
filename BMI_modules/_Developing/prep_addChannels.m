39b0ce2d0f9f703b3d63480378c841f804036aa1
Seonmin Kim <smkim@image.korea.ac.kr>
2016-04-06 13:56:02 +0900

    Update a function 'prep_addChannels.m

--- BMI_modules/_Developing/prep_addChannels.m
+++ BMI_modules/_Developing/prep_addChannels.m
@@ -19,9 +19,40 @@ function [out] = prep_addChannels(dat1, dat2, varargin)
 % Seon Min Kim, 04-2016
 % seonmin5055@gmail.com
 
+if ~isfield(dat1,'x') || ~isfield(dat2,'x')
+    warning('Data is missing: Input data structure must have a field named ''x''')
+    return
+end
+if ~isfield(dat1,'chan') || ~isfield(dat2,'chan')
+    warning('Channel information is missing: Input data structure must have a field named ''chan''')
+    return
+end
+if isempty(varargin)
+    warning('Data of all channels from the latter data will be added to the former data')
+    ch = dat2.chan;
+else
+    ch = varargin{1};
+end
+s1=size(dat1.x);s2=size(dat2.x);
+if s1(1:end-1)~=s2(1:end-1)
+    warning('Unmatched data size')
+    return
+end
+
 out = rmfield(dat1,{'x','chan'});
-ch = varargin{1};
-out.chan = cat(2,dat1.chan,ch);     % It should be checked whether the channels are overlapped
+
+ch_ori = dat1.chan;
+idx = [];
+for i=1:size(ch_ori,2)
+    for j=1:size(ch,2)
+        if strcmp(ch_ori{i},ch{j});
+            idx = [idx,j];
+        end
+    end
+end
+ch(idx) = [];
+
+out.chan = cat(2,dat1.chan,ch);
 ch_idx = find(ismember(dat2.chan,ch));
 d1 = ndims(dat1.x);
 if d1 ==1 || d1 == 2
