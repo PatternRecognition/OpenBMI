32a05c06e39ba87bffb0263dab42506096fa0d1a
JeongJiHoon <jhjeong@image.korea.ac.kr>
2016-05-03 23:18:39 +0900

    small update

--- BMI_modules/_Developing/visualization/plotERSP.m
+++ BMI_modules/_Developing/visualization/plotERSP.m
@@ -1,90 +1,164 @@
 function [ dat ] = plotERSP(data , varargin )
 %UNTITLED 이 함수의 요약 설명 위치
 %   자세한 설명 위치
-
+%% It needs more modify code(?). please kindly wait until updating  
 dat = data;
 opt = opt_CellToStruct(varargin{:});
-
 sampleFrequency=dat.fs;
 %% mem model order?
-modelOrder = 18+round(sampleFrequency/100);
+% modelOrder = 18+round(sampleFrequency/100);
+modelOrder = 20;
 opt.lowPassCutoff=(sampleFrequency/2)-10;
 % highPassCutoff=-1;
 % freqBinWidth=2;
 %  끝이 짤려서 줄임
+opt.highPassCutoff=-1;
 opt.trend=1;
 
-params = [modelOrder, opt.highPassCutoff+opt.freqBinWidth/2, opt.lowPassCutoff-opt.freqBinWidth/2, opt.freqBinWidth,round(opt.freqBinWidth/.2), opt.trend, sampleFrequency];
-memparms = params;
+    spectralSize = round(opt.spectralSize/1000 * sampleFrequency);
+    spectralStep = round(opt.spectralStep/1000 * sampleFrequency);
 
-[nD nTr nCh]=size(dat.x)
-% spectralStep=200;
-% spectralSize=200;
-% datalength=round(nD/spectral_stepping)-1
+    
+% opt.spectralMovStep = fix((size(dat.x,1)-opt.spectralSize)/opt.spectralStep)+1;
+% params = [modelOrder, opt.highPassCutoff+opt.freqBinWidth/2, opt.lowPassCutoff-opt.freqBinWidth/2, opt.freqBinWidth,round(opt.freqBinWidth/.2), ...
+%           opt.trend, sampleFrequency, opt.spectralStep, (opt.spectralSize/opt.spectralStep)];
+params = [modelOrder, opt.highPassCutoff+opt.freqBinWidth/2, opt.lowPassCutoff, opt.freqBinWidth,round(opt.freqBinWidth/.2), ...
+          opt.trend, sampleFrequency];
+% memparms = params;
+% 
+    params(8) = spectralStep;
+    params(9) = spectralSize/spectralStep;
 
-memparms(8) = opt.spectralStep;
-memparms(9) = (opt.spectralSize/opt.spectralStep);% 뒷부분 짤려서 줄임
-%% Here is last update
-idx=find(dat.y_dec==1);
-idx2 = find(SMT.y_dec==2);
-C1=prep_selectTrials(SMT,idx);
-C2=prep_selectTrials(SMT,idx2);
+%% calculating
+
+% C1_idx=find(dat.y_dec==1);
+% C2_idx = find(dat.y_dec==2);
+% C1=prep_selectTrials(dat,C1_idx);
+% C2=prep_selectTrials(dat,C2_idx);
+  C1.x = dat.x(:,1:8,:);
+  
 
 
-
-for i=1:nTr/26
-    dat=C1.x(:,i,:);
-    dat=squeeze(dat);
-    [trialspectrum, freq_bins] = mem( dat, memparms );
-    tm1=mean(trialspectrum, 3);
-    dat_c1(:,:,i)=tm1;
+%% class 1
+for trial=1:size(C1.x,2)
+    plotData1=C1.x(:,trial,:);
+    plotData1=squeeze(plotData1);
+    [trialspectrum, freq_bins] = mem( plotData1, params );
+    tmp=mean(trialspectrum, 3);
+    C1Data(:,:,trial)=tmp;
 end
+freq_bins = freq_bins - opt.freqBinWidth/2;
+C1plotData=mean(C1Data,3);
+% dispmin=min(min(C1plotData));
+% dispmax=max(max(C1plotData));
 
-mean_c1=mean(dat_c1,3);
-xData=freq_bins
+C1plotData=cat(2, C1plotData, zeros(size(C1plotData, 1), 1));
+C1plotData=cat(1, C1plotData, zeros(1, size(C1plotData, 2)));
 
+freq_bins(end+1) = freq_bins(end) + diff(freq_bins(end-1:end));
 
-dispmin=min(min(mean_c1));
-dispmax=max(max(mean_c1));
-num_channels=size(mean_c1, 2);
-data2plot=mean_c1;
-data2plot=cat(2, data2plot, zeros(size(data2plot, 1), 1));
-data2plot=cat(1, data2plot, zeros(1, size(data2plot, 2)));
+% Plot
+figure(1);
+% axis tight;
+surf(freq_bins, [1:size(C1plotData, 2)], C1plotData(1:end,:)');
+% surf(freq_bins, [1:size(C1plotData, 2)], C1plotData');
+axis tight;
+xlim([freq_bins(2) freq_bins(end-1)]); 
+set(gca,'XTick',[freq_bins(2):2: freq_bins(end-1)]);
 
-xData(end+1) = xData(end) + diff(xData(end-1:end));
-surf(xData(4:end), [1:num_channels + 1], data2plot(4:end,:)');
+ylim([1 size(C1Data,2)]);
+set(gca,'YTick',[1:1: size(C1Data,2)]);
+% set(gca,'Yticklabel',char(dat.chan));
+% set(gca,'YTick',[freq_bins(4):2: req_bins(end-1)]);
 view(2);
+
 colormap jet;
 colorbar;
+xlabel('Frequency [Hz]');
+ylabel('Channels');
+title('Frequency band-power per channel');
 
 
 
 %% class 2 
-for i=1:nTr/2
-    dat=C2.x(:,i,:);
-    dat=squeeze(dat);
-    [trialspectrum, freq_bins] = mem( dat, memparms );
-    tm1=mean(trialspectrum, 3);
-    dat_c1(:,:,i)=tm1;
+% bci 2000 data
+C2.x = dat.x(:,9:end,:);
+for trial=1:size(C2.x,2)
+    plotData2=C2.x(:,trial,:);
+    plotData2=squeeze(plotData2);
+    [trialspectrum, freq_bins] = mem( plotData2, params );
+    tmp=mean(trialspectrum, 3);
+    C2Data(:,:,trial)=tmp;
 end
+freq_bins = freq_bins - opt.freqBinWidth/2;
+C2plotData=mean(C2Data,3);
+% dispmin=min(min(C1plotData));
+% dispmax=max(max(C1plotData));
 
-mean_c1=mean(dat_c1,3);
-xData=freq_bins
+C2plotData=cat(2, C2plotData, zeros(size(C2plotData, 1), 1));
+C2plotData=cat(1, C2plotData, zeros(1, size(C2plotData, 2)));
 
+freq_bins(end+1) = freq_bins(end) + diff(freq_bins(end-1:end));
 
-dispmin=min(min(mean_c1));
-dispmax=max(max(mean_c1));
-num_channels=size(mean_c1, 2);
-data2plot=mean_c1;
-data2plot=cat(2, data2plot, zeros(size(data2plot, 1), 1));
-data2plot=cat(1, data2plot, zeros(1, size(data2plot, 2)));
+% Plot
+figure(2);
+% axis tight;
+surf(freq_bins(1:end-1), [1:size(C2plotData, 2)], C2plotData(1:end-1,:)');
+% surf(freq_bins, [1:size(C1plotData, 2)], C1plotData');
 
-xData(end+1) = xData(end) + diff(xData(end-1:end));
-figure;
-surf(xData(4:end), [1:num_channels + 1], data2plot(4:end,:)');
+xlim([freq_bins(1) freq_bins(end-1)]); 
+set(gca,'XTick',[freq_bins(1):2: freq_bins(end-1)]);
+
+ylim([1 size(C2Data,2)]);
+set(gca,'YTick',[1:1: size(C2Data,2)]);
+% set(gca,'Yticklabel',char(dat.chan));
+% set(gca,'YTick',[freq_bins(4):2: req_bins(end-1)]);
 view(2);
+
 colormap jet;
 colorbar;
+xlabel('Frequency [Hz]');
+ylabel('Channels');
+title('Frequency band-power per channel');
+
+%% r square value
+
+  %% r square value
+% ressq = calc_rsqu(double(avgdata1), double(avgdata2), 1);
+% 
+C1Data = double(C1Data); C2Data = double(C2Data);
+for ch=1:size(C1Data, 2)
+    for samp=1:size(C1Data, 1)
+        ressq(samp, ch)=rsqu(C1Data(samp, ch, :), C2Data(samp, ch, :));
+        amp1(samp, ch)=mean(C1Data(samp, ch, :));
+        amp2(samp, ch)=mean(C2Data(samp, ch, :));
+    end
+end
+ressq=cat(2, ressq, zeros(size(ressq, 1), 1));
+ressq=cat(1, ressq, zeros(1, size(ressq, 2)));
+ressq = ressq';
+freq_bins(end+1) = freq_bins(end) + diff(freq_bins(end-1:end));
+
+% Plot
+figure(3);
+% axis tight;
+surf(freq_bins, [1:size(ressq, 2)], ressq(1:end,:)');
+% surf(freq_bins, [1:size(C1plotData, 2)], C1plotData');
+
+xlim([freq_bins(1) freq_bins(end-1)]); 
+set(gca,'XTick',[freq_bins(1):2: freq_bins(end-1)]);
+
+ylim([1 size(ressq,2)]);
+set(gca,'YTick',[1:1: size(ressq,2)]);
+% set(gca,'Yticklabel',char(dat.chan));
+% set(gca,'YTick',[freq_bins(4):2: req_bins(end-1)]);
+view(2);
+
+colormap jet;
+colorbar;
+xlabel('Frequency [Hz]');
+ylabel('Channels');
+title('Frequency band-power per channel');
 
 end
 
--- BMI_modules/_Developing/visualization/prac_ERSP.m
+++ BMI_modules/_Developing/visualization/prac_ERSP.m
@@ -1,23 +1,38 @@
 clear all; close all; clc;
-OpenBMI('E:\Test_OpenBMI\OpenBMI') % Edit the variable BMI if necessary
-global BMI;
-BMI.EEG_DIR=['E:\Test_OpenBMI\BMI_data\RawEEG'];
+% OpenBMI('E:\Test_OpenBMI\OpenBMI') % Edit the variable BMI if necessary
+% global BMI;
+% BMI.EEG_DIR=['E:\Test_OpenBMI\BMI_data\RawEEG'];
+% 
+% %% DATA LOAD MODULE
+% file=fullfile(BMI.EEG_DIR, '\calibration_motorimageryVPkg');
+% marker={'1','left';'2','right';'3','foot';'4','rest'};
+% [EEG.data, EEG.marker, EEG.info]=Load_EEG(file,{'device','brainVision';'marker', marker;'fs', [100]});
+% 
+% %% if you can redefine the marker information after Load_EEG function
+% %% use  [marker, markerOrigin]=prep_defineClass(EEG.marke, marker)
+% 
+% field={'x','t','fs','y_dec','y_logic','y_class','class', 'chan'};
+% CNT=opt_eegStruct({EEG.data, EEG.marker, EEG.info}, field);
+% CNT=prep_selectClass(CNT,{'class',{'right', 'left'}});
+% SMT=prep_segmentation(CNT, {'interval',[0 4000]});
+% a = plotERSP(SMT , {'freqBinWidth' , 2;'spectralSize' , 200; 'spectralStep' , 50; }) ;
+%% Test bci2000 data
+load('E:\Test_OpenBMI\visualization\x1.mat');
+load('E:\Test_OpenBMI\visualization\x2.mat');
+A.x = x1;
+A.fs = 160;
+A.y_dec=1;
+A.x = permute(A.x, [1, 3, 2]);
 
-%% DATA LOAD MODULE
-file=fullfile(BMI.EEG_DIR, '\calibration_motorimageryVPkg');
-marker={'1','left';'2','right';'3','foot';'4','rest'};
-[EEG.data, EEG.marker, EEG.info]=Load_EEG(file,{'device','brainVision';'marker', marker;'fs', [100]});
+B.x = x2;
+B.fs = 160;
+B.y_dec=2;
+B.x = permute(B.x, [1, 3, 2]);
 
-%% if you can redefine the marker information after Load_EEG function
-%% use  [marker, markerOrigin]=prep_defineClass(EEG.marke, marker)
-
-field={'x','t','fs','y_dec','y_logic','y_class','class', 'chan'};
-CNT=opt_eegStruct({EEG.data, EEG.marker, EEG.info}, field);
-CNT=prep_selectClass(CNT,{'class',{'right', 'left'}});
-SMT=prep_segmentation(CNT, {'interval',[0 4000]});
-
-
-a = plotERSP(SMT , {'highPassCutoff' , -1; 'freqBinWidth' , 2; 'spectralStep' , 200; 'spectralSize' , 200}) ;
+C.x = cat(2 , A.x,B.x);
+C.fs = 160;
+C.y = [A.y_dec B.y_dec];
+a = plotERSP(C , {'freqBinWidth' , 2;'spectralSize' , 333; 'spectralStep' , 166; }) ;
 
 
 
