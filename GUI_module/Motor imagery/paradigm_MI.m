4ab7db20b5cd2f35b3846f6fe6dfe6a8e18b3b30
SeonMinKim <smkim@image.korea.ac.kr>
2017-04-11 19:34:26 +0900

    update

--- GUI_module/Motor imagery/paradigm_MI.m
+++ GUI_module/Motor imagery/paradigm_MI.m
@@ -629,3 +629,4 @@ function classType_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
