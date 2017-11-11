d68891c818ffa510fd984e96a74795e041fd9d63
youngeun1209 <youngeun1209@users.noreply.github.com>
2017-11-11 12:22:21 +0900

    update controller

--- GUI_module/Visualization/plot_controller.m
+++ GUI_module/Visualization/plot_controller.m
@@ -1,8 +1,9 @@
-function varargout = plot_controller(dat,varargin)
+function varargout = plot_controller(varargin)
 % PLOT_CONTROLLER MATLAB code for plot_controller.fig
 %      PLOT_CONTROLLER, by itself, creates a new PLOT_CONTROLLER or raises the existing
 %      singleton*.
-%
+%       
+%       excute with plot_controller(SMT)
 %      H = PLOT_CONTROLLER returns the handle to a new PLOT_CONTROLLER or the handle to
 %      the existing singleton*.
 %
@@ -22,7 +23,7 @@ function varargout = plot_controller(dat,varargin)
 
 % Edit the above text to modify the response to help plot_controller
 
-% Last Modified by GUIDE v2.5 10-Nov-2017 11:21:31
+% Last Modified by GUIDE v2.5 10-Nov-2017 20:54:18
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -54,12 +55,13 @@ function plot_controller_OpeningFcn(hObject, eventdata, handles, varargin)
 
 % Choose default command line output for plot_controller
 handles.output = hObject;
-if ~isfield(dat,'x')
+if ~sum(size(varargin))
+    warning('OpenBMI: No data input');return
+elseif ~isfield(varargin{1},'x')
     warning('OpenBMI: Data must have fields named ''x''');return
 else
-    SMT = dat;
+    handles.data=varargin{1};
 end
-opt = opt_cellToStruct(varargin{:});
 % Update handles structure
 guidata(hObject, handles);
 initialize_gui(hObject, handles, false);
@@ -81,6 +83,15 @@ function initialize_gui(fig_handle, handles, isreset)
 % If the metricdata field is present and the reset flag is false, it means
 % we are we are just re-initializing a GUI by calling it from the cmd line
 % while it is up. So, bail out as we dont want to reset the data.
+set(handles.sampling_rate,'String',handles.data.fs);
+set(handles.chan_num,'String',length(handles.data.chan));
+set(handles.time_seg_start, 'String', handles.data.ival(1));
+set(handles.time_seg_end, 'String', handles.data.ival(end));
+chan=handles.data.class{1,2};
+for i=2:size(handles.data.class(:,2))
+    chan=strcat(chan,',',handles.data.class{i,2});
+end
+set(handles.class,'String',chan);
 RESET(handles, true);
 % Update handles structure
 guidata(handles.figure1, handles);
@@ -93,12 +104,17 @@ function RESET(handles, init)
 set(handles.check_time_plot,'Value',true);
 set(handles.check_topography,'Value',true);
 set(handles.chan_listbox, 'String', sprintf('Cz\nOz'));
-set(handles.baseline_start, 'String', -100);
-set(handles.baseline_end, 'String', 0);
-set(handles.freq_band_start, 'String', 7);
-set(handles.freq_band_end, 'String', 13);
-set(handles.time_seg_start, 'String', -200);
-set(handles.time_seg_end, 'String', 1000);
+% set(handles.chan_listbox, 'Value', []);
+% set(handles.baseline_start, 'String', -100);
+% set(handles.baseline_end, 'String', 0);
+
+
+chan=handles.data.class{1,2};
+for i=2:size(handles.data.class(:,2))
+    chan=strcat(chan,'\n',handles.data.class{i,2});
+end
+set(handles.class_listbox,'String',chan);
+
 ival=[0,100;100,200;200,300;300,400;400,500];
 % show_ival='';
 % for i=1:length(ival)
@@ -340,6 +356,118 @@ function baseline_end_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function baseline_end_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to baseline_end (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton6.
+function pushbutton6_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit7_Callback(hObject, eventdata, handles)
+% hObject    handle to edit7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit7 as text
+%        str2double(get(hObject,'String')) returns contents of edit7 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit7_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton7.
+function pushbutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on button press in select_class_btn.
+function select_class_btn_Callback(hObject, eventdata, handles)
+% hObject    handle to select_class_btn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on selection change in class_listbox.
+function class_listbox_Callback(hObject, eventdata, handles)
+% hObject    handle to class_listbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns class_listbox contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from class_listbox
+
+
+% --- Executes during object creation, after setting all properties.
+function class_listbox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to class_listbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit8_Callback(hObject, eventdata, handles)
+% hObject    handle to edit8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit8 as text
+%        str2double(get(hObject,'String')) returns contents of edit8 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit8_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit8 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
