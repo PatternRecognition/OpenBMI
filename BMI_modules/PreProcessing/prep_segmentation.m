5621e500759b51f967b5c6dac88c11180ea5ef98
Oyeon Kwon <oy_kwon@korea.ac.kr>
2017-12-28 14:44:07 +0900

    Update prep_segmentation.m

--- BMI_modules/PreProcessing/prep_segmentation.m
+++ BMI_modules/PreProcessing/prep_segmentation.m
@@ -25,7 +25,7 @@ function [ dat ] = prep_segmentation( dat, varargin )
 % See also 'https://github.com/PatternRecognition/OpenBMI'
 %
 % Min-ho Lee, 12-2017
-% minho_lee@korea.ac.kr
+% mh_lee@korea.ac.kr
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
