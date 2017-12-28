837b3117dcb2f5e0abaa86fc0ce6177183de3c9a
Oyeon Kwon <oy_kwon@korea.ac.kr>
2017-12-28 14:04:48 +0900

    Update prep_selectTrials.m

--- BMI_modules/PreProcessing/prep_selectTrials.m
+++ BMI_modules/PreProcessing/prep_selectTrials.m
@@ -1,19 +1,33 @@
 function [out] = prep_selectTrials(dat,varargin)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % prep_selectTrials (Pre-processing procedure):
-% 
-% Description:
-% This function selects data of specified trials 
-% from continuous or epoched data.
-% 
-% Example:
+%
+% Synopsis:
+%     [out] = prep_selectTrials(DAT,<OPT>)
+%
+% Example :
 %     out = prep_selectTrials(dat,{'Index',[20:35]});
-% 
-% Input: 
-%     dat - Structure. Data which trials are to be selected
-%     index - index of trials to be selected
-% 
-% Seon Min Kim, 04-2016
-% seonmin5055@gmail.com
+%
+% Arguments:
+%     dat - Structure. Continuous data or epoched data
+%         - Data which trials are to be selected   
+%     varargin - struct or property/value list of optional properties:
+%          :  index - index of trials to be selected
+%           
+% Returns:
+%     out - Data structure which has selected channels (continuous or epoched)
+%
+% Description:
+%     This function selects data of specified trials 
+%     from continuous or epoched data.
+%     continuous data should be [time * channels]
+%     epoched data should be [time * channels * trials]
+%
+% See also 'https://github.com/PatternRecognition/OpenBMI'
+%
+% Min-ho Lee, 12-2017
+% minho_lee@korea.ac.kr
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if isempty(varargin)
     warning('OpenBMI: Trials should be specified');
