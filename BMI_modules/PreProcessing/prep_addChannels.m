c4a038791745623fe7f8af7f19b710b25b3e098d
Oyeon Kwon <oy_kwon@korea.ac.kr>
2017-12-28 14:43:08 +0900

    Update prep_addChannels.m

--- BMI_modules/PreProcessing/prep_addChannels.m
+++ BMI_modules/PreProcessing/prep_addChannels.m
@@ -28,7 +28,7 @@ function [out] = prep_addChannels(dat1, dat2, varargin)
 % See also 'https://github.com/PatternRecognition/OpenBMI'
 %
 % Min-ho Lee, 12-2017
-% minho_lee@korea.ac.kr
+% mh_lee@korea.ac.kr
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if isempty(varargin)
     warning('OpenBMI: Data of all channels from the latter data will be added to the former data')
