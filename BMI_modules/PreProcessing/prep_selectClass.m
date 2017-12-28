c17d4679c82ef76b38fddc9db24521c4c75afbff
Oyeon Kwon <oy_kwon@korea.ac.kr>
2017-12-28 14:44:59 +0900

    Update prep_selectClass.m

--- BMI_modules/PreProcessing/prep_selectClass.m
+++ BMI_modules/PreProcessing/prep_selectClass.m
@@ -26,7 +26,7 @@ function [ out ] = prep_selectClass( dat, varargin )
 % See also 'https://github.com/PatternRecognition/OpenBMI'
 %
 % Min-ho Lee, 12-2017
-% minho_lee@korea.ac.kr
+% mh_lee@korea.ac.kr
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if isempty(varargin)
