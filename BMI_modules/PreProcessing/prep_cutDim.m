f2ce7159831517febbe7d9596ff375864d6ce269
Oyeon Kwon <oy_kwon@korea.ac.kr>
2017-12-28 14:43:30 +0900

    Update prep_cutDim.m

--- BMI_modules/PreProcessing/prep_cutDim.m
+++ BMI_modules/PreProcessing/prep_cutDim.m
@@ -24,7 +24,7 @@ function [out] = prep_cutDim(dat)
 % See also 'https://github.com/PatternRecognition/OpenBMI'
 %
 % Min-ho Lee, 12-2017
-% minho_lee@korea.ac.kr
+% mh_lee@korea.ac.kr
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if isstruct(dat)
