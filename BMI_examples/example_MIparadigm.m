eddf6815e06d86e6d9756811c972e0f71c8b7db9
Oyeon Kwon <oy_kwon@korea.ac.kr>
2018-03-19 08:35:07 +0900

    Update example_MIparadigm.m

--- BMI_examples/example_MIparadigm.m
+++ BMI_examples/example_MIparadigm.m
@@ -2,7 +2,7 @@ clear all; clc; close all;
 OpenBMI; % Edit the variable BMI if necessary
 global BMI; % check BMI directories
 
-%% PARADIGM MODULE
+%% PARADIGM MODULE - execute only paradigm
 opt={'Num_trial',10,'Time_STI',2,'Time_ISI',4,'Time_Jitter',0.1,'Num_Screen',1, ...
     'Screen_Size',[1920 1080],'Screen_Type','full', 'Beep', 'on'}
 BMI.IO_ADDR=hex2dec('C010'); %check trigger
