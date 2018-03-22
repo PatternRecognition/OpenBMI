7b8a4f014d4c472a78f5873932d20c5fd43ba994
youngeun1209 <youngeun1209@users.noreply.github.com>
2018-03-22 19:54:44 +0900

    get online data

---
+++ PR_BCI_team/Team_EarEEG/example/get_online_data.m
@@ -0,0 +1,20 @@
+clear all;
+
+%% ear EEG
+% instantiate the library
+disp('Loading the library...');
+lib = lsl_loadlib();
+
+% resolve a stream...
+disp('Resolving an EEG stream...');
+result = {};
+while isempty(result)
+    result = lsl_resolve_byprop(lib,'type','signal');
+end
+
+% create a new inlet
+disp('Opening an inlet...');
+inlet = lsl_inlet(result{1});
+% figure
+disp('Now receiving chunked data...');
+
