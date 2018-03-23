9f5743bb469562ea9ad5cc7c62e86ad5713766dd
ChaeBin <scb0302@hanmail.net>
2018-03-23 11:17:09 +0900

    update

--- PR_BCI_team/Team_Consciousness/Conver2set/1_trialrnd.m
+++ PR_BCI_team/Team_Consciousness/Conver2set/1_trialrnd.m
@@ -1,8 +1,7 @@
 % 1step: trial randomization in the same subject
 % -----------------------------------------------------------------------------------
 % 1-1) struct -> arracy
-% 1-2) This step will be uploaded later..........
-
+% 1-2) modifying the length of epoch: 10sec for each class -> 5 sec
 
 clc; clear; close;
 
@@ -41,4 +40,61 @@ for sub=1:10
     arr_MM_ROC=c_MM_ROC{1};
     arr_MM_REV=c_MM_REV{1};
     
-  
+    %% epoch 수정
+    % WFN 1~1000
+    epo_MM_WFN = arr_MM_WFN(:, (1:1000), :);
+    
+    % LOC 501~1500
+    epo_MM_LOC = arr_MM_LOC(:, (501:1500), :);
+    
+    % UCS 1~1000
+    epo1_MM_UCS = arr_MM_UCS(:, (1:1000), :);
+    
+    % UCS (2) 1001~2000
+    epo2_MM_UCS = arr_MM_UCS(:, (1001:2000), :);
+
+    % ROC 501~1500
+    epo_MM_ROC = arr_MM_ROC(:, (501:1500), :);
+    
+    % REV 1~1000
+    epo_MM_REV = arr_MM_REV(:, (1:1000), :);
+    
+    %% UCS epoch 1과 2 합치기
+    epo_MM_UCS = cat(3, epo1_MM_UCS, epo2_MM_UCS);
+    
+    %% WFN random 추출
+    % WFN trial을 random으로 2개 뽑고, 하나로 합치기
+    MM_WFN_1 = epo_MM_WFN(:, :, randperm(29, 1));
+    MM_WFN_2 = epo_MM_WFN(:, :, randperm(29, 1));
+
+    MM_WFN_rnd = cat(3, MM_WFN_1, MM_WFN_2);    
+    
+    %% LOC random 추출
+    % LOC trial을 random으로 2개 뽑고, 하나로 합치기
+    MM_LOC_1 = epo_MM_LOC(:, :, randperm(3, 1));
+    MM_LOC_2 = epo_MM_LOC(:, :, randperm(3, 1));
+    
+    MM_LOC_rnd = cat(3, MM_LOC_1, MM_LOC_2);  
+    
+    %% UCS random 추출
+    % UCS trial을 random으로 4개 뽑고, 하나로 합치기 
+    MM_UCS_1 = epo_MM_UCS(:, :, randperm(6, 1));
+    MM_UCS_2 = epo_MM_UCS(:, :, randperm(6, 1));
+    MM_UCS_3 = epo_MM_UCS(:, :, randperm(6, 1));
+    MM_UCS_4 = epo_MM_UCS(:, :, randperm(6, 1));
+   
+    MM_UCS_rnd = cat(3, MM_UCS_1, MM_UCS_2, MM_UCS_3, MM_UCS_4);
+    
+    %% ROC random 추출
+    % ROC trial을 random으로 2개 뽑고, 하나로 합치기
+    MM_ROC_1 = epo_MM_ROC(:, :, randperm(3, 1));
+    MM_ROC_2 = epo_MM_ROC(:, :, randperm(3, 1));
+   
+    MM_ROC_rnd = cat(3, MM_ROC_1, MM_ROC_2);  
+    
+    %% REV random 추출
+    % REV trial을 random으로 2개 뽑고, 하나로 합치기
+    MM_REV_1 = epo_MM_REV(:, :, randperm(30, 1));
+    MM_REV_2 = epo_MM_REV(:, :, randperm(30, 1));
+
+    MM_REV_rnd = cat(3, MM_REV_1, MM_REV_2);
