5b3d5c109b59ea114f9f2e33e4ce5248fd54eb48
ChaeBin <scb0302@hanmail.net>
2018-04-26 10:27:24 +0900

    update

--- PR_BCI_team/Team_Consciousness/Conver2set/saveset.m
+++ PR_BCI_team/Team_Consciousness/Conver2set/saveset.m
@@ -1,5 +1,6 @@
 
-% <<<step4>>> mat파일 -> set으로 변환하기
+% <<<step4>>> converting the format of files from mat to set
+% (updating later)
  
 EEG = pop_importdata('dataformat', 'matlab', 'nbchan', 56, 'data', 'D:\anedata_step\d2_MM_1.mat', 'srate', 200, 'pnts', 0, 'xmin', 0);
 
