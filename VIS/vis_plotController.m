30a77674df8a7305b0f3ff28d794f682523ee7e1
hkkim <mykim0409@gmail.com>
2018-05-04 11:03:26 +0900

    Commit

--- VIS/vis_plotController.m
+++ VIS/vis_plotController.m
@@ -1,4 +1,4 @@
-function output = vis_plotController(averaged_SMT, averaged_SMT_r, varargin)
+function output = vis_plotController(averaged_SMT, rval_SMT, varargin)
 % Hong Kyung, Kim
 % hk_kim@korea.ac.kr
 
@@ -26,6 +26,22 @@ end
 if ~isfield(opt, 'Align')
     opt.Align = 'vert';
 end
+if ~isfield(opt, 'TimePlot')
+    opt.TimePlot = 'off';
+end
+if ~isfield(opt, 'TopoPlot')
+    opt.TopoPlot = 'off';
+end
+if ~isfield(opt, 'FFTPlot')
+    opt.FFTPlot = 'off';
+end
+if ~isfield(opt, 'rValue') || isempty(rval_SMT) || isequal(opt.FFTPlot, 'on') % FFT 임시방편
+    opt.rValue = 'off';
+end
+
+if ~sum(strcmpi({opt.TimePlot, opt.TopoPlot, opt.FFTPlot}, 'on'))
+    return;
+end
 
 interval = opt.Interval;
 chan = opt.Channels;
@@ -36,22 +52,15 @@ plt_idx = 1;
 %% Pre-processing
 % [averaged_SMT, averaged_SMT_r] = untitled_function(SMT, opt);
 %% time-domain plot
-if isfield(opt, 'TimePlot') && isequal(opt.TimePlot, 'on')
+if isequal(opt.TimePlot, 'on')
     opt.Plots = plts(plt_idx:plt_idx+length(chan) - 1);
     time_plt = vis_timeAveragePlot(averaged_SMT, opt);
     vis_grpYlabel(time_plt, 'Time-Domain');
     plt_idx = plt_idx + length(time_plt);
 end
-% %% ERDERS plot
-% if isfield(opt, 'ErdPlot') && isequal(opt.ErdPlot, 'on')
-%     opt.Envelope = true;
-%     opt.Plots = plts(plt_idx:plt_idx+length(chan) - 1);
-%     erders_plt = vis_timeAveragePlot(averaged_SMT, opt);
-%     vis_grpYlabel(erders_plt, 'ERD/ERS');
-%     plt_idx = plt_idx + length(erders_plt);
-% end
+
 %% FFT Plot
-if isfield(opt, 'FFTPlot') && isequal(opt.FFTPlot , 'on')
+if isequal(opt.FFTPlot , 'on')
     opt.Plots = plts(plt_idx:plt_idx+size(class,1)*length(chan) - 1);
     fft_plt = vis_freqFFTPlot(averaged_SMT, opt);
     vis_grpYlabel(fft_plt, 'FFT');
@@ -62,16 +71,16 @@ if isfield(opt, 'ErspPlot') && isequal(opt.ErspPlot, 'on')
     %%TODO: Developing the ERSP graph
 end
 %% Topo plot
-if isfield(opt, 'TopoPlot') && isequal(opt.TopoPlot, 'on')
+if isequal(opt.TopoPlot, 'on')
     opt.Plots = plts(plt_idx:plt_idx+size(class,1)*size(interval, 1) - 1);
     topo_plt = vis_topoPlot(averaged_SMT, opt);
     vis_grpYlabel(topo_plt, 'Topography');
     plt_idx = plt_idx + length(topo_plt);
 end
 %% R-value
-if isfield(opt, 'rValue') && isequal(opt.rValue, 'on')
-    opt.Plots = plts(plt_idx:plt_idx+(isequal(opt.TopoPlot, 'on')*size(interval, 1)));
-    r_plt = vis_rValuePlot(averaged_SMT_r, opt);
+if isequal(opt.rValue, 'on')
+    opt.Plots = plts(plt_idx:plt_idx+(isequal(opt.TopoPlot, 'on')*size(interval, 1))-1+isequal(opt.TimePlot, 'on'));
+    r_plt = vis_rValuePlot(rval_SMT, opt);
     vis_grpYlabel(r_plt, 'r-value');
     plt_idx = plt_idx + length(r_plt);
 end
