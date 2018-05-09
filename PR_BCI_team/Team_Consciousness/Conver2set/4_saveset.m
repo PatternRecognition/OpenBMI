b99afd615f1c76a212dbb0b8f0cb649fcf9bb116
ChaeBin <scb0302@hanmail.net>
2018-05-09 20:46:16 +0900

    update

--- PR_BCI_team/Team_Consciousness/Conver2set/4_saveset.m
+++ PR_BCI_team/Team_Consciousness/Conver2set/4_saveset.m
@@ -5,3 +5,4 @@
 EEG = pop_importdata('dataformat', 'matlab', 'nbchan', 56, 'data', 'D:\anedata_step\d2_MM_1.mat', 'srate', 200, 'pnts', 0, 'xmin', 0);
 EEG = pop_importevent(EEG, 'event', 'D:\practice\MM_1_event.txt','fields', {'type' 'latency' 'duration'}, 'skipline', 1, 'timeunit', 1);
 EEG = pop_chanedit(EEG, 'lookup','C:\Users\cvpr\Desktop\code_ane\eeglab13_4_4b\plugins\dipfit2.3\standard_BESA\standard-10-5-cap385.elp','load',{'D:\mjlee_cb\56chansCED.ced' 'filetype' 'autodetect'});
+EEG = pop_saveset( EEG, 'filename','MM_1','filepath','D:\practice');
