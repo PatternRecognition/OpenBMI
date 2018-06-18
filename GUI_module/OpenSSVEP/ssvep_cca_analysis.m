2007ed76cb0bd6f175ab04a196afc8824fbab1b1
hkkim <mykim0409@gmail.com>
2018-06-18 17:38:18 +0900

    Edit cca_analysis

--- GUI_module/OpenSSVEP/ssvep_cca_analysis.m
+++ GUI_module/OpenSSVEP/ssvep_cca_analysis.m
@@ -1,3 +1,26 @@
+% function output = ssvep_cca_analysis(SMT, varargin)
+% in = varargin{:};
+% in = opt_cellToStruct(in);
+% dat = SMT;
+% 
+% nClasses = size(in.marker,1);
+% freq = in.freq;
+% 
+% t= 0:1/in.fs:in.time;
+% Y = cell(1,5);
+% r = cell(1,5);
+% 
+% for i = 1:nClasses
+%     ref = 2*pi*freq(i)*t;
+%     Y{i} = [sin(ref);cos(ref);sin(ref*2);cos(ref*2)];
+% end
+% 
+% for j = 1:nClasses
+%     [~,~,r{j}] = canoncorr(dat,Y{j}');
+%     r{j} = max(r{j});
+% end
+% output = cell2mat(r);
+
 function output = ssvep_cca_analysis(SMT, varargin)
 in = varargin{:};
 in = opt_cellToStruct(in);
@@ -7,8 +30,8 @@ nClasses = size(in.marker,1);
 freq = in.freq;
 
 t= 0:1/in.fs:in.time;
-Y = cell(1,5);
-r = cell(1,5);
+Y = cell(1,nClasses);
+r = cell(1,nClasses);
 
 for i = 1:nClasses
     ref = 2*pi*freq(i)*t;
