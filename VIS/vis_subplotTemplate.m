4f71a473316f091f1cb470ef22c04ef5845b70c1
hkkim <mykim0409@gmail.com>
2018-07-23 17:53:31 +0900

    Fix the renderer issue

--- VIS/vis_subplotTemplate.m
+++ VIS/vis_subplotTemplate.m
@@ -48,6 +48,8 @@ r_plt = gobjects(1, num_rp * num_topo_col + num_rp *  strcmpi(opt.TimePlot, 'on'
 
 fig = figure('Color', 'w');
 set(fig, 'ToolBar', 'none');
+% Rendering Issues
+set(fig, 'RendererMode', 'painters');
 
 monitor_screensize = get(0, 'screensize');
 
