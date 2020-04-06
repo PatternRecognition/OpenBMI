b8e1f286edfc9729d7272352c23c396141a44f68
DKHan <dk_han@korea.ac.kr>
2020-04-06 18:12:20 +0900

    refactor convert_giga_to_mne

--- PR_BCI_team/Team_StarLab/DKHan/examples/Deep Learning Models/Preprocessing/convert_giga_to_mne.py
+++ PR_BCI_team/Team_StarLab/DKHan/examples/Deep Learning Models/Preprocessing/convert_giga_to_mne.py
@@ -23,14 +23,18 @@ for sess in [1,2]:
         raw1, events1 = load_gigadata(MI)
         raw2, events2 = load_gigadata(MI, istrain=False)
 
+        idx = np.array(list(range(7, 11)) + list(range(12, 15)) + list(range(17, 21)) + list(range(32, 41)))
+        chans = np.array(epochs.ch_names)[idx].tolist()
+        epochs.pick_channels(chans)
+
         epochs1 = gigadata_epochs(raw1, events1, tmin=-1., tmax=8.)
         epochs2 = gigadata_epochs(raw2, events2, tmin=-1., tmax=8.)
         epochs = mne.concatenate_epochs([epochs1,epochs2])
-
-        epochs.filter(l_freq=4, h_freq=50)
+        epochs.filter(l_freq=8, h_freq=30)
         epochs.resample(100)
+
         allepo.append(epochs.copy())
 
 
-with open('MI_62ch_100Hz_4-50.pkl', 'wb') as f:
+with open('MI_20ch_100Hz_8-30.pkl', 'wb') as f:
     pickle.dump(allepo, f,protocol=4)
