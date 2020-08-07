eef244266332d174bd6165654818a176367247cb
bh_lee@korea.ac.kr <bh_lee@korea.ac.kr>
2020-08-07 14:06:45 +0900

    Online_RoboticArm_Control_withEEGRecording

--- PR_BCI_team/Team_RobotArm/BHLee/Online_RoboticArm_Control_withEEGRecording.m
+++ PR_BCI_team/Team_RobotArm/BHLee/Online_RoboticArm_Control_withEEGRecording.m
@@ -57,71 +57,93 @@ goToHomePosition(jc);
 current_pos=jc.EndEffectorPose;
 home_pos=jc.EndEffectorPose;
 previous_pos=current_pos;
-
-% 손목 제어는 이 네줄 5,6,7
-% jntVelCmd = [0;0;0;0;0;-0.4;0]; %7DOF
-% for i=1:200
-%     sendJointVelocityCommand(jc,jntVelCmd);
-% end
-
-% limit_x=0.7;
-% limit_y=0.7;
-% limit_z=1.1;
-
-
 %home_pos=[0 0 0 0 0 0];
 %% Desired_pos가 작업범위 바깥이라면 반드시 제외시켜야 한다.
-% home_pos=jc.EndEffectorPose;
-% current_pos=jc.EndEffectorPose;
-% prev_pos=current_pos;
-% 
-% setPositionControlMode(jc);
-% fCmd = 0*ones(3,1);
-% sendFingerPositionCommand(jc,fCmd);
-% 
-% desired_pos=[0.7; -0.25; 0.1; home_pos(4); home_pos(5); home_pos(6)];
-% moveToCP(jc,desired_pos);
-% 
-% 
-% pause(1);
-% 
-% setPositionControlMode(jc);
-% fCmd = 6000*ones(3,1);
-% sendFingerPositionCommand(jc,fCmd);
-% 
-% 
-% desired_pos=[0.7; -0.2; 0.2; home_pos(4); home_pos(5); home_pos(6)];
-% moveToCP(jc,desired_pos);
+home_pos=jc.EndEffectorPose;
+current_pos=jc.EndEffectorPose;
+prev_pos=current_pos;
+
+setPositionControlMode(jc);
+fCmd = 0*ones(3,1);
+sendFingerPositionCommand(jc,fCmd);
+
+desired_pos=[0.7; -0.25; 0.1; home_pos(4); home_pos(5); home_pos(6)];
+moveToCP(jc,desired_pos);
 
 
-% Wrist rotation
-%  jntVelCmd = [0;0;0;0;0;0;0.8]; %7DOF
-%  for i=1:260
-%      sendJointVelocityCommand(jc,jntVelCmd);
-%  end
-% 
-%  jntVelCmd = [0;0;0;0;0;0;-0.8]; %7DOF
-%  for i=1:260
-%      sendJointVelocityCommand(jc,jntVelCmd);
-%  end
-%  
-%  
-%  desired_pos=[0.7; -0.25; 0.1; home_pos(4); home_pos(5); home_pos(6)];
-% moveToCP(jc,desired_pos);
-% 
-% pause(1);
-% 
-% setPositionControlMode(jc);
-% fCmd = 0*ones(3,1);
-% sendFingerPositionCommand(jc,fCmd);
-% 
-% 
-% 
-% desired_pos=[0.2; -0.2; 0.4; home_pos(4); home_pos(5); home_pos(6)];
-% moveToCP(jc,desired_pos);
-% length=sqrt((desired_pos(1)-current_pos(1)).^2+(desired_pos(2)-current_pos(2)).^2+(desired_pos(3)-current_pos(3)).^2);
-% CartVel=0.2;
-% direction=CartVel*[desired_pos(1)-current_pos(1),desired_pos(2)-current_pos(2),desired_pos(3)-current_pos(3)]/length;
-% time=round(length*200/CartVel);
+pause(1);
 
-%time=400;
+setPositionControlMode(jc);
+fCmd = 6000*ones(3,1);
+sendFingerPositionCommand(jc,fCmd);
+
+
+desired_pos=[0.7; -0.2; 0.2; home_pos(4); home_pos(5); home_pos(6)];
+moveToCP(jc,desired_pos);
+
+
+Wrist rotation
+ jntVelCmd = [0;0;0;0;0;0;0.8]; %7DOF
+ for i=1:260
+     sendJointVelocityCommand(jc,jntVelCmd);
+ end
+
+ jntVelCmd = [0;0;0;0;0;0;-0.8]; %7DOF
+ for i=1:260
+     sendJointVelocityCommand(jc,jntVelCmd);
+ end
+ 
+ 
+ desired_pos=[0.7; -0.25; 0.1; home_pos(4); home_pos(5); home_pos(6)];
+moveToCP(jc,desired_pos);
+
+pause(1);
+
+setPositionControlMode(jc);
+fCmd = 0*ones(3,1);
+sendFingerPositionCommand(jc,fCmd);
+
+
+
+desired_pos=[0.2; -0.2; 0.4; home_pos(4); home_pos(5); home_pos(6)];
+moveToCP(jc,desired_pos);
+length=sqrt((desired_pos(1)-current_pos(1)).^2+(desired_pos(2)-current_pos(2)).^2+(desired_pos(3)-current_pos(3)).^2);
+CartVel=0.2;
+direction=CartVel*[desired_pos(1)-current_pos(1),desired_pos(2)-current_pos(2),desired_pos(3)-current_pos(3)]/length;
+time=round(length*200/CartVel);
+
+time=400;
+
+%% i값과 속도값을 적절히 조절하는 것이 관건 
+%% Send cartesian velocity
+%% i=200 은 1초에 해당
+%% 앞의 셋은 미터지만 뒤는 라디안. 5ms마다 입력이 들어가므로 절대 0.01을 넘는 값을 인가하지 말것
+%% 뒤의 셋의 경우 대개 small actuator의 한계값과 관련이 있어 
+
+error=sqrt((desired_pos(1)-current_pos(1)).^2+(desired_pos(2)-current_pos(2)).^2+(desired_pos(3)-current_pos(3)).^2);
+
+while error>0.01
+    temp_pos=jc.EndEffectorPose;
+    error=sqrt((desired_pos(1)-temp_pos(1)).^2+(desired_pos(2)-temp_pos(2)).^2+(desired_pos(3)-temp_pos(3)).^2);
+    CartVel=0.2;
+    direction=CartVel*[desired_pos(1)-temp_pos(1),desired_pos(2)-temp_pos(2),desired_pos(3)-temp_pos(3)]/error;
+    CartVelCmd = [direction(1);direction(2);direction(3);0;0;0];
+    sendCartesianVelocityCommand(jc,CartVelCmd);
+end
+for i=1:time
+temp_pos=jc.EndEffectorPose;
+CartVelCmd = [direction(1);direction(2);direction(3);0;0;0];
+sendCartesianVelocityCommand(jc,CartVelCmd);
+end
+
+current_pos=jc.EndEffectorPose;
+
+%% 이후에 방향 제어라든지 물건을 집는다던지 하는 동작을 수행하여야 한다.
+setPositionControlMode(jc);
+fCmd = 0*ones(3,1);
+sendFingerPositionCommand(jc,fCmd);
+
+pause(3);
+
+setPositionControlMode(jc);
+goToHomePosition(jc);
