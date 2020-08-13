33943adb1c8f0973b838c421a24674faafeed9c3
bh_lee@korea.ac.kr <bh_lee@korea.ac.kr>
2020-08-13 14:09:58 +0900

    Grasping

--- PR_BCI_team/Team_RobotArm/BHLee/Grasping.m
+++ PR_BCI_team/Team_RobotArm/BHLee/Grasping.m
@@ -62,3 +62,33 @@ previous_pos=current_pos;
 setPositionControlMode(jc);
 fCmd = 0*ones(3,1);
 sendFingerPositionCommand(jc,fCmd);
+
+%% EEG_Mat file 받을 준비
+global EEG_MAT_DIR
+EEG_MAT_DIR = '';
+
+dd = 'MotorImagery Converted Data\';
+filelist= {'20191113_demo_bhkwon'};
+
+%%
+Bandpass_Filter = [8 24]; % 어디가 제일 높은 정확도인지 정해야함.
+
+% ----------------------------------------------------------------------
+% Offline Classifier Training
+[cnt, mrk, mnt] = eegfile_loadMatlab([dd filelist{1}]);
+
+ival = [0 3000];
+
+cnt_filt = proc_filtButter(cnt, 5, Bandpass_Filter);
+epo = cntToEpo(cnt_filt,mrk,ival);
+
+[fv, Out.csp_w]=  proc_multicsp(epo, 3);
+fv = proc_variance(fv); fv= proc_logarithm(fv);
+
+fv.classifier_param = {'RLDAshrink','prior', nan, 'store_prior', 1, 'store_means', 1, ...
+    'store_cov', 1, 'store_invcov', 1, 'scaling', 1};
+
+proc = {'wr_multiClass','policy','one-vs-all','coding','hamming'}; % one-vs-all all-pairs
+
+Out.C = trainClassifier(fv, proc);
+Out.out_eeg = applyClassifier(fv, 'wr_multiClass', Out.C);
