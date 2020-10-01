bb2cee2d3d190741da31d1d10d81fac8fc09f39c
DKHan <dk_han@korea.ac.kr>
2020-10-01 16:11:15 +0900

    feat channel selection

--- PR_BCI_team/Team_StarLab/DKHan/examples/Deep_Learning_Models/Dataset/gigadataset.py
+++ PR_BCI_team/Team_StarLab/DKHan/examples/Deep_Learning_Models/Dataset/gigadataset.py
@@ -8,14 +8,13 @@ class GigaDataset(Dataset):
         self.dataset = dataset
         self.len = len(dataset[1])
         self.subj_id = subj_id
-        # self.ch_idx = np.array(list(range(7, 11)) + list(range(12, 15)) + list(range(17, 21)) + list(range(32, 41)))
-
+        #channel selection
+        self.ch_idx = np.array(list(range(7, 11)) + list(range(12, 15)) + list(range(17, 21)) + list(range(32, 41)))
 
     def __len__(self):
         return self.len
     def __getitem__(self, idx):
-
-        X = self.dataset[0][idx,:,:].astype('float32')*1e6
+        X = self.dataset[0][idx,self.ch_idx,:].astype('float32')*1e6
         y = self.dataset[1][idx]
 
         return X, y, self.subj_id
