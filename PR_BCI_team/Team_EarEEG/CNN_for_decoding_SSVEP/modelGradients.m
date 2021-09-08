d1722d5bd0b3419d0f50537a04c20193e0b1a079
youngeun1209 <youngeun1209@users.noreply.github.com>
2021-09-08 18:17:01 +0900

    Create modelGradients.m

---
+++ PR_BCI_team/Team_EarEEG/CNN_for_decoding_SSVEP/modelGradients.m
@@ -0,0 +1,14 @@
+function [gradients, loss] = modelGradients(dlnet, dlX, Y)
+
+    % Forward data through the dlnetwork object.
+    dlY = forward(dlnet,dlX);
+
+    % Compute loss.
+%     loss = crossentropy(dlX,Y);
+    loss = crossentropy(dlY,Y);
+
+    
+    % Compute gradients.
+    gradients = dlgradient(loss,dlnet.Learnables);
+
+end
