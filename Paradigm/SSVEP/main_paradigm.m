e95359e69f0582dd86edfa6da943681f21bb973f
youngeun1209 <youngeun1209@users.noreply.github.com>
2021-09-08 18:11:39 +0900

    Update main_paradigm.m

--- Paradigm/SSVEP/main_paradigm.m
+++ Paradigm/SSVEP/main_paradigm.m
@@ -1,18 +1,3 @@
-%% %%%%%%%%%%%%%%%%  visual Oddball   %%%%%%%%%%%%%%%%%%%%%%%%%%
-trig_vis_erp = [1 2]; % trigger number
-nT = 300; 
-
-% triger, num of tr, comm, screen num(3), session num (shuffle)
-vis_oddball_paradigm_ambulatory(trig_vis_erp, nT,[0 0 0],0, 1); % [0 0 0]: 통신 for cap, ear, IMU
- 
-%% keys setting
-% startKey=KbName('space'); 
-% escapeKey = KbName('esc');
-% waitKey=KbName(']');
-
-% 이름저장 yelee_ERP_0_cap
-% ERP, SSVEP
-% speed: 0,3,6,7
 %% %%%%%%%%%%%%%%%%%%%%%%   SSVEP   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 12, 8.57, 5.45, 3.53 Hz 
 trig_ssvep = [1 2 3]; % standing condition
