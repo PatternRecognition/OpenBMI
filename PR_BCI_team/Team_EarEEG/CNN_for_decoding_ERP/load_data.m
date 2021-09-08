ee14e6772d79a463aa4c758ba0c976cfd7c3e241
youngeun1209 <youngeun1209@users.noreply.github.com>
2021-09-08 18:19:55 +0900

    Update load_data.m

--- PR_BCI_team/Team_EarEEG/CNN_for_decoding_ERP/load_data.m
+++ PR_BCI_team/Team_EarEEG/CNN_for_decoding_ERP/load_data.m
@@ -2,6 +2,6 @@ clear all;
 startup_bbci_toolbox
 
 %% dataload
-dire = 'C:\Users\young\OneDrive\2-PR·¦\ear-EEG\analysis_code\data\2020_competition_data';
-load([dire '\competition_train_test'])
+dire = '\2020_competition_data\';
+load([dire 'competition_train_test'])
 
