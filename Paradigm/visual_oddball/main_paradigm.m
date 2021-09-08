236b8a1845a0e50204bcadd1d0444ef1f1c4c952
youngeun1209 <youngeun1209@users.noreply.github.com>
2021-09-08 18:12:59 +0900

    Update main_paradigm.m

--- Paradigm/visual_oddball/main_paradigm.m
+++ Paradigm/visual_oddball/main_paradigm.m
@@ -3,5 +3,5 @@ trig_vis_erp = [1 2]; % trigger number
 nT = 300; 
 
 % triger, num of tr, comm, screen num(3), session num (shuffle)
-vis_oddball_paradigm_ambulatory(trig_vis_erp, nT,[0 0 0],0, 1); % [0 0 0]: 통신 for cap, ear, IMU
+vis_oddball_paradigm(trig_vis_erp, nT,[0 0 0],0, 1); % [0 0 0]: 통신 for cap, ear, IMU
  
